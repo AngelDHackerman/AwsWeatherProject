977f117c8f0de56db7e7a4c09125d6a128043a35
angelDariaux <angeldariaux@gmail.com>
2023-08-26 13:43:57 -0600

    corrected the errors in the imports

--- AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -1,9 +1,11 @@
-import * as cdk from '@aws-cdk/core'
-import * as lambda from '@aws-cdk/aws-lambda'
-import * as apitgateway from '@aws-cdk/aws-apigateway'
+import { Construct } from 'constructs'
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
-export class WeatherApiStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class AwsWeatherProjectCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     // Define la funcion lambda
@@ -17,13 +19,17 @@ export class WeatherApiStack extends cdk.Stack {
     });
 
     // Define la API Rest en API Gateway
-    const api = new apitgateway.RestApi(this, 'WeatherApi', {
+    const api = new apigateway.RestApi(this, 'WeatherApi', {
       restApiName: 'Weather Service',
     });
 
-    // Crea un nuevo recurso
+    // Creates a 'weather' resource in the API Gateway, representing the route for weather-related API calls.
+    // A GET request to this resource will invoke the 'getWeather' Lambda function.
+    // I will look like this: https://abc123.execute-api.us-east-1.amazonaws.com/weather
+    const weather = api.root.addResource('weather');
+
+    // Agrega un metodo GET al recurso y conecta la funcion lambda
+    weather.addMethod('GET', new apigateway.LambdaIntegration(getWeather))
 
   }
 }
-
-
