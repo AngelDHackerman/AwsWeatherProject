86aafa9d1f95ed90ab8f414e20c862ba36ea89cb
angelDariaux <angeldariaux@gmail.com>
2023-08-25 20:56:00 -0600

    created code for the get Weather lambda

---
+++ getWeather.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => { 
+  const apiKey = process.env.OPENWEATHER_API_KEY || 'default_key'
+
+  // latitude and longitude already defined
+  const lat = 14.64072;
+  const lon = -90.51327;
+
+  try { 
+    const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    return { 
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) { 
+    return { 
+      statusCode: 400,
+      body: 'Unable to fetch weather data',
+    };
+  }
+};
