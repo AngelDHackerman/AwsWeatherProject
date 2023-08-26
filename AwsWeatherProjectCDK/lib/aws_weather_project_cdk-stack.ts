b113e82a3c2f41fcd6082e4f40bc1d3046fc6792
angelDariaux <angeldariaux@gmail.com>
2023-08-26 12:07:56 -0600

    started the CDK project

---
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -0,0 +1,4 @@
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apitgateway from '@aws-cdk/aws-apigateway'
+
---
+++ AwsWeatherProjectCDK/test/aws_weather_project_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsWeatherProjectCdk from '../lib/aws_weather_project_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_weather_project_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsWeatherProjectCdk.AwsWeatherProjectCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
