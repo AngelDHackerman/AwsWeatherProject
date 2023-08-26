c6424a2afe0cde98fc62ea4ece2a391412fa618e
angelDariaux <angeldariaux@gmail.com>
2023-08-26 13:06:53 -0600

    adding the environment variables

--- AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -2,3 +2,21 @@ import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apitgateway from '@aws-cdk/aws-apigateway'
 
+export class WeatherApiStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    // Define la funcion lambda
+    const getWeather = new lambda.Function(this, 'getWeatherFunction', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('../../AwsWeatherProjectOpenWeatherLambda'), // codigo que contiene la lambda que se connecta a OpenWeather
+      handler: 'getWeather.handler', // exporta una función llamada 'handler' en 'getWeather.js'
+      environment: { 
+        OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || 'Valor_por_Defecto'  // Tambien se puede usar el AWS Secrects Manager
+      }
+    })
+
+  }
+}
+
+
