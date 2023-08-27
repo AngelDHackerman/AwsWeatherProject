b06148f66929523d8a06438d44008550668ce32f
angelDariaux <angeldariaux@gmail.com>
2023-08-27 17:30:47 -0600

    adding the Cors headers in the CDK app

--- AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -1,20 +1,20 @@
-import { Construct } from 'constructs'
+import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import * as cdk from 'aws-cdk-lib'
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsWeatherProjectCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+    super(scope, id, props);
 
     // Define la funcion lambda
     const getWeather = new lambda.Function(this, 'getWeatherFunction', {
       runtime: lambda.Runtime.NODEJS_16_X,
-      code: lambda.Code.fromAsset('/home/angel/platziProgramador/AwsWeatherProject/AwsWeatherProjectOpenWeatherLambda'), // codigo que contiene la lambda que se connecta a OpenWeather
-      handler: 'getWeather.handler', // exporta una función llamada 'handler' en 'getWeather.js'
-      environment: { 
-        OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || 'default_value'  // Tambien se puede usar el AWS Secrects Manager
+      code: lambda.Code.fromAsset('/home/angel/platziProgramador/AwsWeatherProject/AwsWeatherProjectOpenWeatherLambda'),
+      handler: 'getWeather.handler',
+      environment: {
+        OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || 'default_value',
       },
     });
 
@@ -23,13 +23,31 @@ export class AwsWeatherProjectCdkStack extends cdk.Stack {
       restApiName: 'Weather Service',
     });
 
-    // Creates a 'weather' resource in the API Gateway, representing the route for weather-related API calls.
-    // A GET request to this resource will invoke the 'getWeather' Lambda function.
-    // I will look like this: https://abc123.execute-api.us-east-1.amazonaws.com/weather
+    // Creates a 'weather' resource in the API Gateway
     const weather = api.root.addResource('weather');
 
     // Agrega un metodo GET al recurso y conecta la funcion lambda
-    weather.addMethod('GET', new apigateway.LambdaIntegration(getWeather))
+    const getWeatherMethod = weather.addMethod('GET', new apigateway.LambdaIntegration(getWeather), {
+      methodResponses: [{
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Origin': true,
+        },
+      }],
+      // @ts-ignore
+      integrationResponses: [{
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Origin': "'*'",
+        },
+      }],
+    });
 
+    // Add CORS options
+    weather.addCorsPreflight({
+      allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      allowMethods: apigateway.Cors.ALL_METHODS,
+      allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+    });
   }
 }
