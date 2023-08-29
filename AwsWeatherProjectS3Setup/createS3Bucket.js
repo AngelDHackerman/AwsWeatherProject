2ac880a3ecfa2c6770c914c20c6f2bc9667c7741
angelDariaux <angeldariaux@gmail.com>
2023-08-28 20:17:46 -0600

    added parameters for create the bucket

---
+++ AwsWeatherProjectS3Setup/createS3Bucket.js
@@ -0,0 +1,47 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const AWS = __importStar(require("aws-sdk"));
+// configura las credenciales y la region
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-east-1'
+});
+// Crea una nueva instancia del servicio S3
+const s3 = new AWS.S3();
+// Parametros para la creacion del bucket
+const bucketParams = {
+    Bucket: 'weatherProjectFrontendBucket',
+    CreateBucketConfiguration: {
+        LocationConstraint: 'us-east-1'
+    }
+};
+// Crea el bucket
--- AwsWeatherProjectS3Setup/createS3Bucket.ts
+++ AwsWeatherProjectS3Setup/createS3Bucket.ts
@@ -5,6 +5,19 @@ import * as AWS from 'aws-sdk'
 // configura las credenciales y la region
   AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-east-1'
   });
-  
+
+// Crea una nueva instancia del servicio S3
+const s3 = new AWS.S3();
+
+// Parametros para la creacion del bucket
+const bucketParams: AWS.S3.CreateBucketRequest = { 
+  Bucket: 'weatherProjectFrontendBucket',
+  CreateBucketConfiguration: { 
+    LocationConstraint: 'us-east-1'
+  }
+};
+
+// Crea el bucket
