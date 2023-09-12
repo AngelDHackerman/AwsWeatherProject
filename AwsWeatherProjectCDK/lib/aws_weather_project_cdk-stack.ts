82e311e410601aecb5afda7f81a340bc22eba658
angelDariaux <angeldariaux@gmail.com>
2023-09-11 21:26:42 -0600

    created the resource for the api gateway

--- AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -5,5 +5,34 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'; 
 
 export class AwsWeatherProjectCdkStack extends cdk.Stack { 
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) { 
+    super(scope, id, props);
+
+    // Inyectamos la funcion lambda como una dependencia
+    const getWeather = this.createLambdaFunction();
+
+    // Inyectamos la API Gateway como una dependencia
+    const api = this.createApiGateway();
+
+    // Creamos un recurso 'weather' en la API Gateway.
+    const weather = api.root.addResource('weather')
+
+    // Agregamos un metodo GET al recurso y conectamos la funcion lambda
+    this.addGetMethodToResource(weather, getWeather)
+  }
+
+  // Metodo para crear la funcion lambda
+  private createLambdaFunction(): lambda.Function { 
+    return new lambda.Function(this, 'getWeatherFunction', { 
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('/home/angel/platziProgramador/AwsWeatherProject/AwsWeatherProjectOpenWeatherLambda'),
+      handler: 'getWeather.handle',
+      environment: { 
+        OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || 'default_value',
+      },
+    });
+  }
+
+  // Metodo para crear la API Gateway
   
 }
