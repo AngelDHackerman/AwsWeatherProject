363d328c2d72444ba06fd82a8efd05808e098c3d
angelDariaux <angeldariaux@gmail.com>
2023-09-11 21:15:43 -0600

    starting the imports

---
+++ AwsWeatherProjectCDK/lib/aws_weather_project_cdk-stack.ts
@@ -0,0 +1,9 @@
+import { Construct } from "constructs";
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'; 
+
+export class AwsWeatherProjectCdkStack extends cdk.Stack { 
+  
+}
